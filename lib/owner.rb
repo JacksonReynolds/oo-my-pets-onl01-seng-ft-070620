52eb5d27c7a4d1594441d7e151d2b6f5bcffa082
Jackson Reynolds <jwreynold@gmail.com>
2020-07-20 20:20:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -49,7 +49,11 @@ class Owner
       dog.owner = nil
       dog.mood = 'nervous'
     end #do
-  end
+  end #sell_pets
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end #list_pets
 
 #CLASS METHODS
   def self.all
